0acc3b64fc9db83a1737856fb120834ccaa4bc29
UmaiyaalChellam <meenakshibaskar18@gmail.com>
2017-06-22 10:35:31 +0530

    Create facts.java

---
+++ facts.java
@@ -0,0 +1,16 @@
+package factorials;
+
+public class facts {
+	public static void main(String[] args)
+	{
+		int i;
+		int fact=1;
+		int number=5;
+		for(i=1;i<=number;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println("Factorial of a given number is:" +fact);
+	}
+
+}
