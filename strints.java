38f2998a18949a6093cae73157b11f50ae8fa191
UmaiyaalChellam <meenakshibaskar18@gmail.com>
2017-06-22 12:15:26 +0530

    Create strints.java

---
+++ strints.java
@@ -0,0 +1,11 @@
+package strint;
+
+public class strints {
+          public static void main(String[] args)
+          {
+        	  String s="3";
+        	 int x= Integer.parseInt(s);
+        	 System.out.println(s+100);
+        	 System.out.println(x+100);
+          }
+}
